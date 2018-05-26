312f0b1faf22af8d2899fdb97a3de51bd444fd56
Bogdan Nedelcu <bogdan.nedelcu@lokalleads.de>
2018-05-26 20:09:08 +0200

    src vimeo api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import api from "./api"
+import test from "./test"
+import processor from "./process"
+import video from './videoApi'
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props)
+    this.state = {
+      links: [],
+      text: ''
+    }
+  }
+
+  async componentDidMount(){
+    const entities = await api.analyzeEntities(test.real, 'entities')
+    const processed = processor.processEntities(entities)
+
+    const query = processed && processed.ORGANIZATION.map(p => p.name)
+    console.log(`${query[0]} and ${query[1]}`, "query")
+    const links = await video(`${query[0]}`)
+    this.setState({
+      links
+    })
+  }
+  
+  render () {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,8 +35,14 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <div dangerouslySetInnerHTML={this.state.links[0]} style={{height: "400px"}}>
+        </div>
+        <div>{JSON.stringify(this.state.text)}</div>
+        
       </div>
     );
   }
---
+++ src/test.js
@@ -0,0 +1,41 @@
+export default {
+    megan: `
+    One month ago, Travel & Leisure reported a source confirmed that Meghan Markle 
+    and Prince Harry would be going to Namibia on their honeymoon—something People 
+    echoed in its report that the luxury Hoanib Valley Camp was one likely option 
+    for the couple. But even if that was the original plan, as soon as it became 
+    public and reported widely across outlets, that changed fast. 
+    E! reported right after the wedding that Namibia was not the destination. 
+    And royal expert Omid Scobie, who was ABC News's royal contributor during 
+    the wedding, confirmed that really any destination the press catches wind of is out.`,
+    real: `
+        Real Madrid faces Liverpool on Saturday in the UEFA Champions League final in Kiev, Ukraine. 
+        Madrid is in the final four the third year in a row, and the fourth time in five years. 
+        Liverpool last played in the final in 2007.
+        What to Expect in the Champions League Final
+        • Goals. Liverpool set a tournament record this season with 46. Their player to watch is Mohamed Salah,
+        the Egyptian striker who led the Premier League in scoring and has 44 goals in all competitions. 
+        Real Madrid counters with its Portuguese star Cristiano Ronaldo, the leading scorer in Champions 
+        League history (120 goals in 152 appearances).
+        
+        • Ronaldo scored 15 goals in 12 Champions League games this season. One more on Saturday would
+        make him the only player to score in four Champions League finals.
+        
+        • Real Madrid is looking to extend its record by winning a 13th Champions League title, and to become
+        the first team since Bayern Munich (1974-76) to claim three in a row.
+        
+        Continue reading the main story
+        • “We’ll never live this again,” Real Madrid Manager Zinedine Zidane said Friday. “We’ll live other 
+        moments but we’ll never have today and tomorrow again. We need to take advantage. If there’s pressure,
+        well, that’s life. It makes it better.”
+        
+        • Liverpool is chasing its sixth trophy, which would move it ahead on Bayern Munich and Barcelona on
+        the all-time list. Only Madrid and A.C. Milan, with seven, have more. The Reds’ last visit to the final 
+        was in 2007 (a loss to Milan). Their last triumph was in 2005, when Liverpool rallied from a 3-0 halftime 
+        deficit to beat Milan on penalties — a game that has become known as the Miracle of Istanbul.
+        
+        • “This club has it in its DNA to win big things,” Liverpool Manager Jurgen Klopp said Friday. 
+        “We are here because we are Liverpool. We cannot try to fight on their level, but tactics in football 
+        are there to bring an opponent onto your level.”
+        `
+}

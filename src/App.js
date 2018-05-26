534afa1a9fc64c3aa92538fe62b22496b35c8669
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2018-05-26 19:16:27 +0200

    latest styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import SendText from "./components/SendText";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <SendText />
-      </div>
+      </Container>
     );
   }
 }
--- src/components/SendText.js
+++ src/components/SendText.js
@@ -1,5 +1,48 @@
 import React from "react";
 import "../App.css";
+import styled from "styled-components";
+import { Button } from "semantic-ui-react";
+
+const Header = styled.div`
+  width: 100%;
+  font-family: "Lobster", cursive;
+  color: white;
+  background-color: #2ecc71;
+  display: flex;
+  padding: 0.5rem 1rem;
+  justify-content: space-between;
+  align-items: flex-end;
+  align-content: flex-end;
+`;
+
+const Body = styled.div`
+  padding: 0.5rem 1rem;
+`;
+
+const Logo = styled.div`
+  font-size: 30px;
+`;
+
+const PageTitle = styled.div`
+  display: flex;
+`;
+
+const Menu = styled.div`
+  display: flex;
+`;
+
+const MenuItem = styled.div`
+  padding: 0.5rem;
+`;
+
+const Root = styled.div`
+  width: 100%;
+  max-width: 1100px;
+`;
+
+const StyledButton = styled(Button)`
+  font-family: "Lobster", cursive !important;
+`;
 
 class Form extends React.Component {
   constructor(props) {
@@ -21,23 +64,38 @@ class Form extends React.Component {
   };
 
   render() {
-    console.log(this.state);
     return (
-      <div className="container">
-        <div className="row justify-content-md-center">
-          <div className="col-12">
-            <h2>Structured Content</h2>
-            <form onSubmit={this.onSubmit} className="form">
-              <div className="form-group">
-                <textarea onChange={this.onChange} className="form-control" />
-                <button type="submit" className="btn btn-primary">
-                  Analyze
-                </button>
-              </div>
-            </form>
+      <Root>
+        <Header>
+          <Logo>captio.io</Logo>
+          <Menu>
+            <MenuItem>Analyze</MenuItem>
+            <MenuItem>Process</MenuItem>
+            <MenuItem>Download</MenuItem>
+          </Menu>
+        </Header>
+        <Body>
+          <div className="row justify-content-md-center">
+            <div className="col-12">
+              <PageTitle>
+                <h3>Structured Content</h3>
+              </PageTitle>
+              <form onSubmit={this.onSubmit} className="form">
+                <div className="form-group">
+                  <textarea
+                    onChange={this.onChange}
+                    className="form-control"
+                    style={{ width: "100%" }}
+                  />
+                  <StyledButton primary type="submit">
+                    Analyze
+                  </StyledButton>
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
-      </div>
+        </Body>
+      </Root>
     );
   }
 }

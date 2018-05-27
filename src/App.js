7cf038fe142fce5c2593f6a462d5bdf27c6a14d2
Bogdan Nedelcu <bogdan.nedelcu@lokalleads.de>
2018-05-27 10:00:10 +0200

    push

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import video from "./videoApi";
 import React, { Component } from "react";
 import SendText from "./components/SendText";
 import styled, { ThemeProvider } from "styled-components";
+import axel from './axelContent'
 
 const Container = styled.div`
   display: flex;
@@ -33,9 +34,11 @@ class App extends Component {
     const entities = await api.analyzeEntities(test.real, "entities");
     const processed = processor.processEntities(entities);
 
+    axel()
+
     const query = processed && processed.ORGANIZATION.map(p => p.name);
-    console.log(`${query[0]} and ${query[1]}`, "query");
     const links = await video(`${query[0]}`);
+    
     this.setState({
       links
     });
@@ -46,10 +49,6 @@ class App extends Component {
       <ThemeProvider theme={theme}>
         <Container>
           <SendText />
-          <div
-            dangerouslySetInnerHTML={this.state.links[0]}
-            style={{ height: "400px" }}
-          />
           <div>{JSON.stringify(this.state.text)}</div>
         </Container>
       </ThemeProvider>
@@ -58,3 +57,9 @@ class App extends Component {
 }
 
 export default App;
+
+
+// <div
+//   dangerouslySetInnerHTML={this.state.links[0]}
+//   style={{ height: "400px" }}
+// />}
--- src/axelContent.js
+++ src/axelContent.js
@@ -1,10 +1,21 @@
+import { Base64 } from 'js-base64';
+
+const query = 'obama'
+const url = `https://sandbox-api.ipool.asideas.de/linguistics/entities?q=${query}&publisher="WELT"`
+
+export default async function(){
 
 
-export default axel = async function(){
+    const response = await fetch(url, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "Authorization": `Basic ${Base64.encode('user' + ':' + 'password')}`,
+            "content-type": 'application/json; charset=UTF-8'
+        }
+    }).then(data => data.json()).then(data => console.log(data))
 
 
 
-
-
-    return
+    return 'asdasd'
 }

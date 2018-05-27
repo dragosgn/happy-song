7608449635672a0b1ab8bfad1ab9640c19d70e10
mohammad.seyedsaidi <mirnumbing@gmail.com>
2018-05-27 10:02:47 +0200

    getting api

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import api from "./api";
 import test from "./test";
 import processor from "./process";
 import video from "./videoApi";
-import React, { Component } from "react";
+import React, {Component} from "react";
 import SendText from "./components/SendText";
-import styled, { ThemeProvider } from "styled-components";
+import styled, {ThemeProvider} from "styled-components";
+
+
 
 const Container = styled.div`
   display: flex;
@@ -15,46 +17,47 @@ const Container = styled.div`
 `;
 
 const theme = {
-  primary: "#2ecc71",
-  secondary: "#007bff",
-  terciary: "#0062cc"
+    primary: "#2ecc71",
+    secondary: "#007bff",
+    terciary: "#0062cc"
 };
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      links: [],
-      text: ""
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            links: [],
+            text: ""
+        };
+    }
 
-  async componentDidMount() {
-    const entities = await api.analyzeEntities(test.real, "entities");
-    const processed = processor.processEntities(entities);
+    async componentDidMount() {
+        const entities = await api.analyzeEntities(test.real, "entities");
+        console.log(entities)
+        const processed = processor.processEntities(entities);
 
-    const query = processed && processed.ORGANIZATION.map(p => p.name);
-    console.log(`${query[0]} and ${query[1]}`, "query");
-    const links = await video(`${query[0]}`);
-    this.setState({
-      links
-    });
-  }
+        const query = processed && processed.ORGANIZATION.map(p => p.name);
+        console.log(`${query[0]} and ${query[1]}`, "query");
+        const links = await video(`${query[0]}`);
+        this.setState({
+            links
+        });
+    }
 
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <Container>
-          <SendText />
-          <div
-            dangerouslySetInnerHTML={this.state.links[0]}
-            style={{ height: "400px" }}
-          />
-          <div>{JSON.stringify(this.state.text)}</div>
-        </Container>
-      </ThemeProvider>
-    );
-  }
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <Container>
+                    <SendText/>
+                    <div
+                        dangerouslySetInnerHTML={this.state.links[0]}
+                        style={{height: "400px"}}
+                    />
+                    <div>{JSON.stringify(this.state.text)}</div>
+                </Container>
+            </ThemeProvider>
+        );
+    }
 }
 
 export default App;

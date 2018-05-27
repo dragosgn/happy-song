63572af765de59f1f64cf2a6fb3c0eb09f8b348e
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2018-05-27 10:50:09 +0200

    fixing imports

--- src/components/styled.js
+++ src/components/styled.js
@@ -1,8 +1,25 @@
 import React from "react";
 
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Button = styled.button`
+  padding: 0.25rem 1.5rem;
+  cursor: pointer;
+  font-size: 1.5rem;
+  background-color: ${props =>
+    props.color ? props.color : props.theme.secondary};
+  color: white;
+  border: 1px solid
+    ${props => (props.color ? props.color : props.theme.secondary)};
+  border-radius: 2px;
+  font-family: "Lobster", cursive;
+  :hover {
+    background-color: ${props =>
+      props.secondaryColor ? props.secondaryColor : props.theme.terciary};
+  }
+`;
+
+export const buttonCss = styled.css`
   padding: 0.25rem 1.5rem;
   cursor: pointer;
   font-size: 1.5rem;
